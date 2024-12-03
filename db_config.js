8cfc6d678143690021e3f929ade61c57451e5117
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-12-03 09:55:06 +0100

    add db_config

---
+++ db_config.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mysql from "mysql2";
+
+// Database configuration
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+};
+
+// Create a connection
+const connection = mysql.createConnection(dbConfig);
+
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Connection error:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database!');
+});
+
+export default connection;
