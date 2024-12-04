685c94c10b16a303ac9ebe4ac525a24a46e17d1a
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-12-04 16:10:19 +0100

    refactor: améliorer la lisibilité et la structure du code dans orangeAuth.js

--- orangeAuth.js
+++ orangeAuth.js
@@ -1,52 +1,49 @@
-require('dotenv').config();
+require("dotenv").config();
 
 let toRefresh = true;
 let timerID = false;
 let token;
 
 async function fetchOrangeToken() {
-    try {
-        const response = await fetch('https://api.orange.com/oauth/v3/token', {
-            method: 'POST',
-            headers: {
-                'Authorization': process.env.ORANGE_KEY,
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'application/json',
-            },
-            body: new URLSearchParams({
-                'grant_type': 'client_credentials'
-            })
-        });
-        if (!response.ok) {
-            throw new Error(`not daijoubu: ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Error fetching token:', error);
-    }
+	try {
+		const response = await fetch("https://api.orange.com/oauth/v3/token", {
+			method: "POST",
+			headers: {
+				"Authorization": process.env.ORANGE_KEY,
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Accept": "application/json",
+			},
+			body: new URLSearchParams({
+				"grant_type": "client_credentials",
+			}),
+		});
+		if (!response.ok) {
+			throw new Error(`not daijoubu: ${response.status}`);
+		}
+		return await response.json();
+	} catch (error) {
+		console.error("Error fetching token:", error);
+	}
 }
 
-
-    
-function refreshIn(){
-    if(!timerID)
-        clearTimeout(timerID)
-    timerID = setTimeout(() => {
-        toRefresh = true;
-      }, "3500 second");
-    return timerID;
+function refreshIn() {
+	if (!timerID) clearTimeout(timerID);
+	timerID = setTimeout(() => {
+		toRefresh = true;
+	}, "3500 second");
+	return timerID;
 }
 
-async function getOrangeToken(){
-    if(toRefresh){
-        token = await fetchOrangeToken();
-        toRefresh = false;
-        timerID = refreshIn();
-    }
-console.log(token)
-console.log("timerID = " + timerID)
-console.log("refresh state = " + toRefresh)
-    return token
+async function getOrangeToken() {
+	if (toRefresh) {
+		token = await fetchOrangeToken();
+		toRefresh = false;
+		timerID = refreshIn();
+	}
+	console.log(token);
+	console.log("timerID = " + timerID);
+	console.log("refresh state = " + toRefresh);
+	return token;
 }
 
 getOrangeToken();
