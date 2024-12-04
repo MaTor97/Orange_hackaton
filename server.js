2f38fe7b9c6c4d2898ba418e6bca4ef3106ca8de
Aliabieva Diana <aliabievadiana25@gmail.com>
2024-12-04 14:03:57 +0100

    way to open map.hnml

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.post('/submit', (req, res) => {
         if (err) {
             return res.status(500).send('Error inserting data into the database.');
         }
-        // window.location.href = './public/map.html';
-        res.send('Data inserted successfully!');
+         res.status(200).redirect('/map/map.html');
     });
 });
 
